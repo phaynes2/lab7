65590f00b4977d94240715ee7981e40a9c28e72f
phaynes2 <phaynes2@hawk.iit.edu>
2019-04-16 16:39:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ function init(){
 //add your javascrip between these two lines of code
   var click = document.getElementById("entrybutton");
   click.addEventListener('click', function() {
-    var info = document.getElementById("entryinput");
-    alert ("Paul Haynes: " + info.value);
-    });
-}
+    var info = document.getElementById("entryinput").value;
+    alert ("Paul Haynes: " + info);
+    console.log(info);
+    document.getElementById("textoutput").innerHTML = info;
+  });}
 window.addEventListener('load', init);
