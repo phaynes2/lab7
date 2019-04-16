3e4dda6a6767365f02d7bc70984996fa8efe8ee8
phaynes2 <phaynes2@hawk.iit.edu>
2019-04-16 16:51:34 -0500

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
-  var click = document.getElementById("entrybutton");
-  click.addEventListener('click', function() {
-    var info = document.getElementById("entryinput").value;
-    alert ("Paul Haynes: " + info);
-    console.log(info);
-    document.getElementById("textoutput").innerHTML = info;
-  });}
+var button = document.getElementById("entrybutton");
+button.addEventListener('click', function () {
+var message = document.getElementById("entryinput").value;
+
+alert("Phallyn Habercoss: " + message);
+console.log(message);
+document.getElementById("textoutput").innerHTML = message;
+});
+}
 window.addEventListener('load', init);
+
